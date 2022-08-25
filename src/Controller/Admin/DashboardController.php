2d32ab2720b63fe858d2a54d035fcefe20c21337
steeven-js <kisama972@gmail.com>
2022-08-25 16:23:34 -0400

    025 Installation, configuration et mapping de EasyAdmin avec l'entité User()

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use App\Entity\Product;
 use App\Entity\Category;
 use App\Controller\Admin\UserCrudController;
 use Symfony\Component\HttpFoundation\Response;
@@ -52,6 +53,7 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Users', 'fas fa-user', User::class);
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Categories', 'fas fa-list', Category::class);
+        yield MenuItem::linkToCrud('Products', 'fas fa-tag', Product::class);
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
     }
 }
