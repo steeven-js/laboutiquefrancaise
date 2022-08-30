16e9fc7e42ac08cb09d047ce0317eb1fa7237347
steeven-js <kisama972@gmail.com>
2022-08-30 12:23:00 -0400

    037 Création du panier et de notre classe Cart()

--- src/Classe/Cart.php
+++ src/Classe/Cart.php
@@ -2,6 +2,49 @@
 
 namespace App\Classe;
 
-class Cart {
+use Symfony\Component\HttpFoundation\RequestStack;
+//use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+
+class Cart 
+
+{
+	//private $session;
+	private $requestStack;
+
+	//public function __construct(SessionInterface $session)
+	public function __construct(RequestStack $requestStack)
+	{
+	//$this->session = $session;
+	$this->requestStack = $requestStack;
+	}
+
+
+	public function add($id)
+	{
+		$cart = $this->requestStack->getSession()->get('cart', []);
+
+		if (!empty($cart[$id])) {
+        	$cart[$id]++; 
+		} else {
+			$cart[$id] = 1;
+		}
+
+		$this->requestStack->getSession()->set('cart',$cart); 
+	}
+
+    // AFFICHE le panier
+	public function get()
+	{
+		
+		return $this->requestStack->getSession()->get('cart');
+	}
+
+    // SUPPRIME le panier
+	public function remove()
+	{
+		
+		return $this->requestStack->getSession()->remove('cart');
+	}
 
 }
--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,22 +2,33 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Classe\Cart;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CartController extends AbstractController
 {
+    // Mon panier
     #[Route('/mon-panier', name: 'app_cart')]
     public function index(): Response
     {
         return $this->render('cart/index.html.twig');
     }
 
-    #[Route('/cart/add/{id}', name: 'app_cart')]
-    public function add($id): Response
+    // Ajout
+    #[Route('/cart/add/{id}', name: 'add_to_cart')]
+    public function add(Cart $cart, $id): Response
     {
-        dd($id);
-        return $this->render('cart/index.html.twig');
+        $cart->add($id);
+        return $this->redirectToRoute('app_cart');
+    }
+
+    // Suppression
+    #[Route('/cart/remove', name: 'remove_my_cart')]
+    public function remove(Cart $cart): Response
+    {
+        $cart->remove();
+        return $this->redirectToRoute('app_cart');
     }
 }
