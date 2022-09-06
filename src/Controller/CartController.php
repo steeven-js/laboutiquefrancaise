77a2e4a1d7019c49ab7f0a512077c15511f72fdb
steeven-js <kisama972@gmail.com>
2022-09-06 10:01:31 -0400

    039 Ajout, diminution et suppression de produit dans panier

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,8 +3,6 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
-use App\Entity\Product;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
